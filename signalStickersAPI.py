96165aa695bfcd4e72fc6b865eeb81714bcabcf2
fayzsa <fayz.sabir@gmail.com>
2021-01-31 22:33:40 +0100

    upload

--- signalStickersAPI.py
+++ signalStickersAPI.py
@@ -37,14 +37,13 @@ async def main(packTitle, packAuthor, listofStick, coverStick):
     # - GIF <100kb for animated stickers
     for i in listofStick:
         add_sticker(i, "🤪")
-    # add_sticker("D:\Tuto12\Programming\PyProjects\stick1.webp", "🤪")
 
     # Specifying a cover is optionnal
     # By default, the first sticker is the cover
     cover = Sticker()
     cover.id = pack.nb_stickers
     # Set the cover file here
-    with open("D:\Tuto12\Programming\PyProjects\stick1.webp", "rb") as f_in:
+    with open(coverStick, "rb") as f_in:
         cover.image_data = f_in.read()
     pack.cover = cover
 
@@ -52,6 +51,10 @@ async def main(packTitle, packAuthor, listofStick, coverStick):
     async with StickersClient('165f3c42-886c-4017-a620-f3199c049d49.2', 'T7dYGpKVqVCc90DP5cMeyg') as client:
         # Upload the pack
         pack_id, pack_key = await client.upload_pack(pack)
+
+# How to obtain your Signal credentials? In Signal Desktop, open the Developer Tools, and type
+# window.reduxStore.getState().items.uuid_id to get your USER
+# and  window.reduxStore.getState().items.password to get your PASSWORD.
 
     print("Pack uploaded!\n\nhttps://signal.art/addstickers/#pack_id={}&pack_key={}".format(pack_id, pack_key))
     link = {
@@ -65,10 +68,11 @@ async def main(packTitle, packAuthor, listofStick, coverStick):
     )
     return response
 
+
 @app.route('/makeSticks/', methods=['POST'])
 def create_Stickers():
-    D = asyncio.run(main(request.json['packName'], request.json['authorName'], [
-                     'D:\Tuto12\Programming\PyProjects\stick1.webp', 'D:\Tuto12\Programming\PyProjects\stick1.webp'], 'D:\Tuto12\Programming\PyProjects\stick1.webp'))
+    D = asyncio.run(main(request.json['packName'], request.json['authorName'],
+                         request.json['listOfSticks'], request.json['cover']))
 
     # D = asyncio.run(main(request.json['packName'],  request.json['authorName'],
     #      request.json['listofStick'], request.json['cover']))
